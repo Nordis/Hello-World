a9ed2530fbeb8eb4bf3e97e80a342eba845c9a07
Daniel Nordmark <nordmark.daniel@gmail.com>
2011-10-01 14:09:11 +0200

    Hello World is a useless class

---
+++ Hello-World/Hello-World/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿namespace Hello_World
+{
+    /// <summary>
+    /// A completely useless class
+    /// </summary>
+    public static class HelloWorld
+    {
+        public static string Message
+        {
+            get { return "Hello World!"; }
+        }
+    }
+}
